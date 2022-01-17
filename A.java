1a6ba85bef17c7af3593f1cfe88af4c6ea24aedb
lucky26jixiang <luckyjixiang@hotmail.com>
2022-01-17 23:43:30 +0900

    new

--- A.java
+++ A.java
@@ -2,9 +2,17 @@ import java.util.Scanner;
 class A {
 	int lineNumber =2;
 	public void move(){
-	int a=2;
 	}
 }
 class B {
-	int b_a=2;
+	int a=2;
+	int b=2;
+}
+class C{
+	int c=5;
+	int myNum=c+2;
+	for(i = 0; i < 5; i++) {
+		System.out.println(i);
+	}
+	System.out.println{myNum};
 }
