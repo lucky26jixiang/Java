996801d8241096ccb7b3dc61759bcade12955311
lucky26jixiang <luckyjixiang@hotmail.com>
2022-03-01 16:24:04 +0900

    ad

--- A.java
+++ A.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 class A {
     public void move(){
 	int a=2;
+	int lineNumber =2;
     }
 }
 class B {
     int b=2;
     int c=5;
-    int lineNumber =2;
+  
 }
 class C{
     int myNum=c+2;
-
-    for(i = 5; i > 0; i--) {
-	int dn=3;
-	dn+=5;
+    for(i = 0; i > 5; i++) {
+	int d=3;
+	d+=5;
 	System.out.println(i);
 	System.out.println(a);
     }
