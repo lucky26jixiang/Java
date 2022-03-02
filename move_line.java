c4a513e1f000e27ff2700576eb5424b94ed54502
lucky26jixiang <luckyjixiang@hotmail.com>
2022-03-02 17:22:03 +0900

    create

---
+++ move_line.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class A {
+    int lineNumber =2;
+    public void move(){
+	int a=2;
+    }
+}
+class B {
+    int b=2;
+    int c=5;
+}
+class C{
+    int myNum=c+2;
+    for(i = 0; i < 5; i++) {
+	int d=3;
+	d+=5;
+	System.out.println(i);
+    }
+    System.out.println{myNum};
+}
