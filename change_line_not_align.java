bac1de81fe595586abefd1c27bad117527edf752
lucky26jixiang <luckyjixiang@hotmail.com>
2022-03-02 17:26:56 +0900

    change

--- change_line_not_align.java
+++ change_line_not_align.java
@@ -10,10 +10,11 @@ class B {
     int c=5;
 }
 class C{
+	
     int myNum=c+2;
-    for(i = 0; i < 5; i++) {
-	int d=3;
-	d+=5;
+    for(i = 5; i < 0; i--) {
+	int dn=3;
+	dn+=5;
 	System.out.println(i);
     }
     System.out.println{myNum};
