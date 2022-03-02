1a5b13793d8cc0efc3809cbaddebc6cfccbb426b
lucky26jixiang <luckyjixiang@hotmail.com>
2022-03-02 17:21:31 +0900

    create

---
+++ change_line_align.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class A {
+    public void move(){
+	int a=2;
+    }
+}
+class B {
+    int b=2;
+    int c=5;
+    int lineNumber =2;
+}
+class C{
+    int myNum=c+2;
+
+    for(i = 5; i > 0; i--) {
+    int dn=3;
+    dn+=5;
+    int en=400000;
+	System.out.println(i);
+	System.out.println(a);
+    }
